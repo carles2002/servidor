26f8e060a086d11307185b362d6f8b1e64ba8b62
Carles <cmarfem@upv.edu.es>
2023-10-02 14:37:43 +0200

    Se almacena en la bd

--- bd/controlador.php
+++ bd/controlador.php
@@ -1,4 +1,23 @@
 <?php
-include("bd.php");
+
+//----------------------------------------------------------------
+// Realiza la consulta SQL para obtener los datos de la base de datos
+$sql = "SELECT major, minor FROM datos_beacon";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Si se encontraron resultados, obtén el primer resultado
+    $row = $result->fetch_assoc();
+
+    // Almacena los valores en variables de sesión
+    $_SESSION['clave1'] = $row['clave1'];
+    $_SESSION['clave2'] = $row['clave2'];
+
+    // Cierra la conexión a la base de datos
+    $conn->close();
+} else {
+    echo "No se encontraron resultados en la base de datos.";
+}
 
 ?>
--- bd/recibir_json.php
+++ bd/recibir_json.php
@@ -6,9 +6,32 @@ $data = json_decode($json_data);
 $clave1 = $data->clave1;
 $clave2 = $data->clave2;
 
-// Realiza las operaciones necesarias en el servidor
+// Establece la conexión con la base de datos
+$servername = "localhost"; // Cambia esto por la dirección de tu servidor MySQL
+$username = "root";
+$password = "";
+$dbname = "proyecto3a";
 
-// Devuelve una respuesta (opcional)
-$respuesta = array('mensaje' => 'Datos recibidos correctamente');
-echo json_encode($respuesta);
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica la conexión
+if ($conn->connect_error) {
+    die("Error en la conexión a la base de datos: " . $conn->connect_error);
+}
+
+// Prepara la consulta SQL para insertar los datos en la tabla
+$sql = "INSERT INTO testB (major, minor) VALUES ('$clave1', '$clave2')";
+
+if ($conn->query($sql) === TRUE) {
+    // Los datos se insertaron correctamente en la base de datos
+    $respuesta = array('mensaje' => 'Datos recibidos y guardados correctamente en la base de datos');
+    echo json_encode($respuesta);
+} else {
+    // Hubo un error al insertar los datos
+    $respuesta = array('mensaje' => 'Error al guardar los datos en la base de datos: ' . $conn->error);
+    echo json_encode($respuesta);
+}
+
+// Cierra la conexión a la base de datos
+$conn->close();
 ?>
