1847341bb1c523ad42805eb5879b473175f2f711
Carles <cmarfem@upv.edu.es>
2023-10-02 14:48:35 +0200

    Se lee el ultimo dato y se muestra

--- bd/controlador.php
+++ bd/controlador.php
@@ -1,23 +1,33 @@
 <?php
+// Establece la conexión con la base de datos (reemplaza los valores según tu configuración)
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "proyecto3a";
 
-//----------------------------------------------------------------
-// Realiza la consulta SQL para obtener los datos de la base de datos
-$sql = "SELECT major, minor FROM datos_beacon";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica la conexión
+if ($conn->connect_error) {
+    die("Error en la conexión a la base de datos: " . $conn->connect_error);
+}
+
+// Realiza la consulta SQL para obtener la última entrada de la tabla testB
+$sql = "SELECT * FROM testB ORDER BY id DESC LIMIT 1"; // Ordena por ID de forma descendente y limita a 1 resultado
 
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // Si se encontraron resultados, obtén el primer resultado
+    // Si se encontraron resultados, devuelve la última entrada como un array asociativo
     $row = $result->fetch_assoc();
-
-    // Almacena los valores en variables de sesión
-    $_SESSION['clave1'] = $row['clave1'];
-    $_SESSION['clave2'] = $row['clave2'];
-
-    // Cierra la conexión a la base de datos
-    $conn->close();
 } else {
-    echo "No se encontraron resultados en la base de datos.";
+    // Si no se encontraron entradas en la tabla testB, devuelve un array vacío
+    $row = array();
 }
 
+// Cierra la conexión a la base de datos (puedes cerrarla en otro lugar si es necesario)
+$conn->close();
+
+// Devuelve el resultado como un JSON
+echo json_encode($row);
 ?>
--- user/inicio.php
+++ user/inicio.php
@@ -1,61 +1,64 @@
 <?php
-//Si no has iniciado sesión, no puedes entrar a la página de inicio
-/*session_start();
-if(!isset($_SESSION['usuario'])){
-    header("Location:../user/login.php");
-}else{
-    if($_SESSION['ususario']=="ok"){
-
-    }
-    $nombre = $_SESSION['nombre'];
-    echo "El nombre del usuario es: " . $nombre;
-}*/
 session_start();
 ?>
 
 <!doctype html>
 <html lang="en">
 
-    <head>
-        <title>Title</title>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    </head>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
 
-    <body>
-        <?php $url="http://".$_SERVER['HTTP_HOST']?>
-        <nav class="navbar navbar-expand navbar-light bg-light">
-            <div class="nav navbar-nav">
-                <a class="nav-item nav-link active" href="#"> Bienvenido, 
+<body>
+    <?php $url = "http://" . $_SERVER['HTTP_HOST'] ?>
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <div class="nav navbar-nav">
+            <a class="nav-item nav-link active" href="#"> Bienvenido,
                 <?php echo $_SESSION['usuario']; ?> </a>
-                <a class="nav-item nav-link" href="<?php echo $url;?>/user/inicio.php">Inicio</a>
-                <a class="nav-item nav-link" href="<?php echo $url;?>/user/cerrar.php">Cerrar sesión</a>
-                <a class="nav-item nav-link" href="<?php echo $url;?>">Ver sitio web</a>
-            </div>
-        </nav>
+            <a class="nav-item nav-link" href="<?php echo $url; ?>/user/inicio.php">Inicio</a>
+            <a class="nav-item nav-link" href="<?php echo $url; ?>/user/cerrar.php">Cerrar sesión</a>
+            <a class="nav-item nav-link" href="<?php echo $url; ?>">Ver sitio web</a>
+        </div>
+    </nav>
 
-        <br>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="jumbotron">
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="jumbotron">
+                    <h1 class="display-3">Mediciones</h1>
+                    <p class="lead">La medición de su sonda es: </p>
+                    <hr class="my-2">
+                    <p>More info</p>
+                    <p class="lead">
+                        <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+                    </p>
+                    <?php
+                    // Incluye la conexión y datos desde el archivo db_connection.php
+                    include '../bd/controlador.php';
 
-                        <h1 class="display-3">Mediciones</h1>
-                        <p class="lead">La medición de su sonda es: </p>
-                        <hr class="my-2">
-                        <p>More info</p>
-                        <p class="lead">
-                            <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
-                        </p>
-                    </div>
+                    if (!empty($row)) {
+                        echo "<h2>Última entrada de la tabla testB:</h2>";
+                        echo "<p>ID: " . $row["ID"] . "</p>";
+                        echo "<p>Campo1: " . $row["major"] . "</p>"; // Reemplaza "campo1" con el nombre de tu columna
+                        echo "<p>Campo2: " . $row["minor"] . "</p>"; // Reemplaza "campo2" con el nombre de tu columna
+                        // Agrega más campos según sea necesario
+                    } else {
+                        echo "<p>No se encontraron entradas en la tabla testB.</p>";
+                    }
+                    ?>
                 </div>
             </div>
         </div>
+    </div>
 
-    </body>
+</body>
 
 </html>
