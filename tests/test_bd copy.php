aa2369c7cda13103ce5b2c2edd63018d58d45d77
Carles <carlesmartif@gmail.com>
2023-10-16 16:36:55 +0200

    Nuevos test por consola implementados

--- tests/test_bd copy.php
+++ tests/test_bd copy.php
@@ -1,4 +1,3 @@
-
 <?php
 
 // Test: Insertar, leer y borrar datos
@@ -8,46 +7,49 @@ $password = "";
 $dbname = "proyecto3a";
 
 $pasados = 0;
+$errores = [];
 
-//Hacer la conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verifica la conexión
-if ($conn->connect_error) {
-    die("❌Error en la conexión a la base de datos: " . $conn->connect_error. PHP_EOL);
+try {
+    // Hacer la conexión
+    $conn = new mysqli($servername, $username, $password, $dbname);
     
-}
-
-// 1. Insertar datos en la base de datos
-$test_data = "INSERT INTO medicion (`id sonda`, fecha, hora, Latitud, Longitud, Humedad, Temperatura, Contaminacion) VALUES ('33','2023-01-01','00:33:00','33','33','31','31', '34')";
-
-if ($conn->query($test_data) === TRUE) {
+    // Verifica la conexión
+    if ($conn->connect_error) {
+        throw new Exception("Error en la conexión a la base de datos");
+    }
+    
+    // 1. Insertar datos en la base de datos
+    $test_data = "INSERT INTO medicion (`id sonda`, fecha, hora, Latitud, Longitud, Humedad, Temperatura, Contaminacion) VALUES ('33','2023-01-01','00:33:00','33','33','31','31', '34')";
+    
+    if ($conn->query($test_data) !== TRUE) {
+        throw new Exception("al insertar el dato de prueba");
+    }
     echo "✅ Dato de prueba insertado con éxito.". PHP_EOL;
-    nl2br(2);
-$pasados ++;
+    $pasados++;
     
-} else {
-    echo "❌ Error al insertar el dato de prueba: " . $conn->error . PHP_EOL;
-    nl2br(2);
-}
-
-// 2. Leer los datos (ya está implementado arriba)
-
-// 3. Borrar los datos insertados
-$delete_test_data = "DELETE FROM medicion WHERE `id sonda`='33' AND fecha='2023-01-01' AND hora='00:33:00'";
-if ($conn->query($delete_test_data) === TRUE) {
+    // 3. Borrar los datos insertados
+    $delete_test_data = "DELETE FROM medicion WHERE `id sonda`='33' AND fecha='2023-01-01' AND hora='00:33:00'";
+    if ($conn->query($delete_test_data) !== TRUE) {
+        throw new Exception("al eliminar el dato de prueba");
+    }
     echo "✅ Dato de prueba eliminado con éxito". PHP_EOL;
-    nl2br(2);
-    $pasados ++;
-} else {
-    echo "❌Error al eliminar el dato de prueba: " . $conn->error. PHP_EOL;
-    nl2br(2);
+    $pasados++;
+    
+} catch (Exception $e) {
+    $errores[] = $e->getMessage();
 }
 
+// Reportar resultados del test
+echo PHP_EOL . "Resumen de tests:". PHP_EOL;
+if ($pasados > 0) {
+    echo "✅ $pasados tests pasados correctamente". PHP_EOL;
+}
 
-if ($pasados =2){
-    echo "✅ ✅TODOS LOS TESTS PASADOS CORRECTAMENTE". PHP_EOL;
-    nl2br(2);
+if (count($errores) > 0) {
+    echo PHP_EOL . "Errores detectados:". PHP_EOL;
+    foreach ($errores as $error) {
+        echo "❌ Error en: " . $error . PHP_EOL;
+    }
 }
 
 ?>
--- tests/test_bd_Usuarios copy.php
+++ tests/test_bd_Usuarios copy.php
@@ -1,46 +1,54 @@
-
 <?php
 
-// Test: Insertar, leer y borrar datos
+// Test: Insertar, leer y borrar datos en la tabla 'usuarios'
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "proyecto3a";
 
-$pasados =0;
+$pasados = 0;
+$errores = [];
 
-//Hacer la conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verifica la conexión
-if ($conn->connect_error) {
-    die(" ❌ Error en la conexión a la base de datos: " . $conn->connect_error);
-}
-
-// 1. Insertar datos en la base de datos
-$test_data = "INSERT INTO usuarios (nombre, usuario,contraseña,idSonda) VALUES ('hola','holaUsuario','holaContraseña','33')";
-if ($conn->query($test_data) === TRUE) {
-    echo "✅ Dato de prueba insertado con éxito.". PHP_EOL;
-   $pasados++;
+try {
+    // Hacer la conexión
+    $conn = new mysqli($servername, $username, $password, $dbname);
     
-} else {
-    echo "Error al insertar el dato de prueba: " . $conn->error . PHP_EOL;
+    // Verifica la conexión
+    if ($conn->connect_error) {
+        throw new Exception("Error en la conexión a la base de datos");
+    }
+    
+    // 1. Insertar datos en la base de datos
+    $test_data = "INSERT INTO usuarios (nombre, usuario, contraseña, idSonda) VALUES ('hola','holaUsuario','holaContraseña','33')";
+    if ($conn->query($test_data) !== TRUE) {
+        throw new Exception("al insertar el dato de prueba en 'usuarios'");
+    }
+    echo "✅ Dato de prueba insertado con éxito.". PHP_EOL;
+    $pasados++;
+    
+    // 3. Borrar los datos insertados
+    $delete_test_data = "DELETE FROM usuarios WHERE nombre='hola' AND usuario='holaUsuario' AND contraseña='holaContraseña' AND idSonda='33'";
+    if ($conn->query($delete_test_data) !== TRUE) {
+        throw new Exception("al eliminar el dato de prueba en 'usuarios'");
+    }
+    echo "✅ Dato de prueba eliminado con éxito". PHP_EOL;
+    $pasados++;
+    
+} catch (Exception $e) {
+    $errores[] = $e->getMessage();
 }
 
-// 2. Leer los datos (ya está implementado arriba)
-
-// 3. Borrar los datos insertados
-$delete_test_data = "DELETE FROM usuarios WHERE nombre='hola' AND usuario='holaUsuario' AND contraseña='holaContraseña' AND idSonda='33'";
-if ($conn->query($delete_test_data) === TRUE) {
-    echo "✅ Dato de prueba eliminado con éxito.". PHP_EOL;
-    $pasados ++;
-} else {
-    echo "❌ Error al eliminar el dato de prueba: " . $conn->error . "<br>". PHP_EOL;
+// Reportar resultados del test
+echo PHP_EOL . "Resumen de tests:". PHP_EOL;
+if ($pasados > 0) {
+    echo "✅ $pasados tests pasados correctamente". PHP_EOL;
 }
 
-if ($pasados =2){
-    echo "✅ ✅TODOS LOS TESTS PASADOS CORRECTAMENTE". PHP_EOL;
-    nl2br(2);
+if (count($errores) > 0) {
+    echo PHP_EOL . "Errores detectados:". PHP_EOL;
+    foreach ($errores as $error) {
+        echo "❌ Error en: " . $error . PHP_EOL;
+    }
 }
 
 ?>
